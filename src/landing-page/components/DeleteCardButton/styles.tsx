1b391fcc675786b9449bed721c02f0d40288117e
juunini <juuni.ni.i@gmail.com>
2021-10-28 18:53:42 +0900

    Add button style

--- src/landing-page/components/DeleteCardButton/styles.tsx
+++ src/landing-page/components/DeleteCardButton/styles.tsx
@@ -1,7 +1,14 @@
 import styled from '@emotion/styled';
+import { Button as MuiButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-export const Button = styled.button`
+export const Button = styled(MuiButton)`
 
 `;
+Button.defaultProps = {
+  variant: 'contained',
+  color: 'error',
+  startIcon: <DeleteIcon />,
+};
 
 export default Button;

badc82475af6851201489e151785890d82f96bb9
juunini <juuni.ni.i@gmail.com>
2021-10-28 17:33:10 +0900

    Add DeleteCardButton with Card component

--- src/landing-page/components/Cards/test.tsx
+++ src/landing-page/components/Cards/test.tsx
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Cards from '.';
 
 describe('Cards Component is', () => {
   const givenValues: string[] = ['card1', 'card2'];
+
+  context('When click delete button', () => {
+    const onClick: () => void = jest.fn();
+
+    it('Should be calls onClick', () => {
+      render(<Cards onClick={onClick} values={givenValues} />);
+
+      givenValues.forEach((_, index: number) => {
+        fireEvent.click(screen.getAllByRole('button')[index]);
+
+        expect(onClick).toBeCalled();
+      });
+    });
+  });
 
   it('Should renders given values to Card components', () => {
     render(<Cards values={givenValues} />);
